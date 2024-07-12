d783e88e160d6e5d3747afac440d102b6e11bf10
Romi <qq1694821929@gmail.com>
2024-07-13 04:22:31 +0800

    Update

--- lib/ObjectivesFunctions/BasicFunctions.hpp
+++ lib/ObjectivesFunctions/BasicFunctions.hpp
@@ -4,30 +4,6 @@
 
 #ifndef BASICFUNCTIONS_HPP
 #define BASICFUNCTIONS_HPP
-void CreateAudioBlock() {
-    //    get user input name
-    std::string name;
-    std::cout << "Enter the name of the audio block: ";
-    std::cin >> name;
-
-    EntityBlockCore block(EntityBlockType_Audio);
-    block.SetName(name);
-    std::cout << "Block Name: " << block.GetName() << std::endl;
-    std::cout << "Block Pan: " << block.GetPan() << std::endl;
-    std::cout << "Block Vol: " << block.GetVol() << std::endl;
-    std::cout << "Block Type: " << block.GetTypeName() << std::endl;
-}
-void CreateMIDIBlock() {
-    //    get user input name
-    std::string name;
-    std::cout << "Enter the name of the midi block: ";
-    std::cin >> name;
-
-    EntityBlockCore block(EntityBlockType_Midi);
-    block.SetName(name);
-    std::cout << "Block Name: " << block.GetName() << std::endl;
-    std::cout << "Block Pan: " << block.GetPan() << std::endl;
-    std::cout << "Block Vol: " << block.GetVol() << std::endl;
-    std::cout << "Block Type: " << block.GetTypeName() << std::endl;
-}
+void CreateAudioBlock();
+void CreateMIDIBlock();
 #endif //BASICFUNCTIONS_HPP
